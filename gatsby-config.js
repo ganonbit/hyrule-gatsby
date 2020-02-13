0adfbec830aa1e4e81d647cbf794f2d925f1fd2f
Andrew Reese <areese@270b.com>
2020-02-13 17:56:55 -0600

    making it mine slowly via metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,14 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    title: "hyrule",
-    author: "ΔNDRΞW",
-    description: "alabama blood, texas heart. progressive. coder. gamer. music lover. foodie. 🖤zelda & penguins🖤",
+    siteTitle: "hyrule",
+    siteTitleAlt:"hyrule - @hyliancoder",
+    siteHeadline: "hyrule - @hyliancoder",
+    author: "@hyliancoder",
+    siteDescription: "alabama blood, texas heart. progressive. coder. gamer. music lover. foodie. 🖤zelda & penguins🖤",
     siteUrl: "https://hyliancoder.com/",
+    siteImage: "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/84269546_10162900818350481_6003901046110289920_o.jpg?_nc_cat=106&_nc_ohc=n3mH5TXp3YcAX-pqx9h&_nc_ht=scontent-dfw5-2.xx&oh=a98d5fec75539e2bc74f671407cf5a40&oe=5EC1A8BF",
+    siteLanguage: "EN",
     social: [
       {
         name: "twitter",
@@ -38,12 +42,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `ΔNDRΞW @hyliancoder`,
+        short_name: `ΔNDRΞW`,
+        description: `site for hyliancoder.com`,
         start_url: `/`,
         background_color: `#141821`,
-        theme_color: `#f6ad55`,
+        theme_color: `#502F6D`,
         display: `standalone`,
         icons: [
           {
